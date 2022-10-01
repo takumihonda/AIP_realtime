c529ffa458664e455dbbd387fa0f81bd4319862f
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 03:22:38 +0000

    Adding tarfile member sanitization to extractall()

--- realtime_20200825/leadtime_from_tar.py
+++ realtime_20200825/leadtime_from_tar.py
@@ -54,7 +54,26 @@ def untar():
     for tf in tfile_l:
         print( tf )
         with tarfile.open( tf, 'r' ) as tar:
-            tar.extractall( os.path.join( path, "tmp") )
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, os.path.join(path,"tmp"))
 
 
 def get_leadtime( file_l ):
